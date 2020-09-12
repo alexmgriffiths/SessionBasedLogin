3c23de29ca6af25a401fc4d3a1c4d3de8c3ec8ad
Alex Griffiths <67096118+alexmgriffiths@users.noreply.github.com>
2020-09-11 20:24:14 -0500

    Update global.php

--- global.php
+++ global.php
@@ -5,7 +5,7 @@ session_start();
 ini_set("display_errors", 1);
 error_reporting(E_ALL ^ E_NOTICE);
 /*
- * Change to this for production use.
+ * Change to this for production use. Not that any of this should be used in production.
  * ini_set("display_errors", 0);
  * error_reporting(0);
  */
